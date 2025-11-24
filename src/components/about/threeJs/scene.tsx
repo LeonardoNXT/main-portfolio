ab0df0290fb5ef9634d83080aecc9c4d7fc2bb86
Leonardo Leal <Leonardoffice12@gmail.com>
2025-11-24 17:32:50 -0300

    FIX: Performance Leaks ThreeJs are been corrected.

--- src/components/about/threeJs/scene.tsx
+++ src/components/about/threeJs/scene.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { RefObject, useRef } from "react";
+import { JSX, RefObject, useEffect, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap/gsap-core";
 import SceneContent from "./sceneContent";
@@ -23,6 +23,21 @@ export default function Scene({
   const cameraPosition = useRef({ x: -20, y: 5, z: 2 });
   const cameraLookAt = useRef({ x: 0, y: 10, z: 0 });
   const heightScreen = window.innerHeight;
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    function calcIsMobile() {
+      const isMobileVarible = window.innerWidth < 768;
+      setIsMobile(isMobileVarible);
+    }
+    calcIsMobile();
+
+    window.addEventListener("resize", calcIsMobile);
+
+    return () => {
+      window.removeEventListener("resize", calcIsMobile);
+    };
+  }, []);
 
   useGSAP(
     (context) => {
@@ -75,41 +90,48 @@ export default function Scene({
       style={{ background: "#ffffff" }}
       dpr={Math.min(window.devicePixelRatio, 2)}
       gl={{
-        antialias: true,
+        antialias: !isMobile,
         alpha: true,
         toneMapping: THREE.ACESFilmicToneMapping,
         toneMappingExposure: 0.5,
         outputColorSpace: THREE.SRGBColorSpace,
       }}
-      onCreated={({ gl }) => {
-        gl.shadowMap.enabled = true;
-        gl.shadowMap.type = THREE.PCFSoftShadowMap;
-      }}
     >
-      {/* FOG - ISSO ESTAVA FALTANDO! */}
-      <fogExp2 attach="fog" args={[0xffffff, 0.01]} />
+      {!isMobile && <fogExp2 attach="fog" args={[0xffffff, 0.01]} />}
 
       <SceneContent
         cameraPosition={cameraPosition}
         cameraLookAt={cameraLookAt}
+        isMobile={isMobile}
       />
       <color attach="background" args={["#ffffff"]} />
-      {/* Environment SEM background para não sobrescrever o fog */}
+
       <Environment
         files="/panoramic.hdr"
         background={false}
         environmentIntensity={1}
       />
 
-      {/* EffectComposer na ordem correta */}
-      <EffectComposer multisampling={4}>
+      <EffectComposer multisampling={isMobile ? 0 : 4}>
         <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
-        <Noise opacity={0.6} blendFunction={BlendFunction.DARKEN} premultiply />
-        <Vignette
-          eskil={false}
-          offset={0.3} // intensidade do escurecimento nas bordas
-          darkness={0.7} // quão escuro fica
-        />
+        {
+          (!isMobile && (
+            <Noise
+              opacity={0.6}
+              blendFunction={BlendFunction.DARKEN}
+              premultiply
+            />
+          )) as JSX.Element
+        }
+        {
+          (!isMobile && (
+            <Vignette
+              eskil={false}
+              offset={0.3} // intensidade do escurecimento nas bordas
+              darkness={0.7} // quão escuro fica
+            />
+          )) as JSX.Element
+        }
       </EffectComposer>
     </Canvas>
   );
--- src/components/about/threeJs/sceneContent.tsx
+++ src/components/about/threeJs/sceneContent.tsx
@@ -7,9 +7,11 @@ type CameraRef = RefObject<{ x: number; y: number; z: number }>;
 export default function SceneContent({
   cameraPosition,
   cameraLookAt,
+  isMobile,
 }: {
   cameraPosition: CameraRef;
   cameraLookAt: CameraRef;
+  isMobile: boolean;
 }) {
   const { camera, scene } = useThree();
 
@@ -35,43 +37,36 @@ export default function SceneContent({
   return (
     <>
       {/* Luz direcional principal */}
-      <directionalLight
-        position={[50, 10, -30]}
-        intensity={1.0}
-        color={0xffaa66}
-        castShadow
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
-        shadow-camera-near={0.1}
-        shadow-camera-far={50}
-        shadow-camera-left={-20}
-        shadow-camera-right={20}
-        shadow-camera-top={20}
-        shadow-camera-bottom={-20}
-        shadow-normalBias={0.05}
-        shadow-bias={-0.001}
-      />
+      {!isMobile && (
+        <directionalLight
+          position={[50, 10, -30]}
+          intensity={1.0}
+          color={0xffaa66}
+          shadow-mapSize-width={512}
+          shadow-mapSize-height={512}
+          shadow-camera-near={0.1}
+          shadow-camera-far={50}
+          shadow-camera-left={-20}
+          shadow-camera-right={20}
+          shadow-camera-top={20}
+          shadow-camera-bottom={-20}
+          shadow-normalBias={0.05}
+          shadow-bias={-0.001}
+        />
+      )}
 
       {/* HemisphereLight */}
       <hemisphereLight args={[0xffccaa, 0x333366, 1.0]} />
 
       {/* Luz do sol (DirectionalLight forte) */}
-      <directionalLight
-        position={[20, 80, 20]}
-        intensity={5}
-        castShadow
-        shadow-mapSize-width={4096}
-        shadow-mapSize-height={4096}
-        shadow-camera-near={10}
-        shadow-camera-far={200}
-        shadow-camera-left={-50}
-        shadow-camera-right={50}
-        shadow-camera-top={50}
-        shadow-camera-bottom={-50}
-        shadow-bias={-0.0002}
-        shadow-normalBias={0.02}
-        shadow-radius={0.5}
-      />
+      {!isMobile && (
+        <directionalLight
+          position={[-10, 30, -5]}
+          intensity={1.5}
+          color={0xffddcc}
+          castShadow={false}
+        />
+      )}
 
       {/* Luz ambiente */}
       <ambientLight intensity={0.3} />
